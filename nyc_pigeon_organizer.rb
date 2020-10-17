3cbff28e6e573a5dbecfb197377013c6b74136d6
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-17 19:35:52 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,8 @@
 def nyc_pigeon_organizer(data)
   pig_hash = data 
-  
+  pigeon_list = {}
+  pig_hash.each do |k,v|
+    if k == :color 
+      for i in 0...pig_hash[k][v].size do 
+        pigeon_list{pig_hash[k][v].size[i]}
 end
