77c1f2b600936147e2064cd7c5ad15448b9c249a
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-17 20:06:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,14 +1,4 @@
 def nyc_pigeon_organizer(data)
-  pig_hash = data 
-  pigeon_list = {}
-  pig_hash.each do |k,v|
-    if k == :color 
-      for i in 0...pig_hash[k][v].size do
-        if pigeon_list[pig_hash[k][v][i]] == nil 
-          pigeon_list[pig_hash[k][v][i]] => k
-          pigeon_list[pig_hash[k][v][i]][k] = [v.to_s]
-        else 
-        pigeon_list{pig_hash[k][v][i]} => :color
   data.each_with_object({}) do |k,v,array|
     v.each do |inner_k,name|
       if !array[name]
