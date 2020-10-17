1223b174e754e974ba8f1055e93593f3ce2c82a2
Brendan O'Connell <bmoconnell15@gmail.com>
2020-10-17 20:22:31 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   final = data.each_with_object({}) do |(k,v), array|
     v.each do |inner_k,name|
+      name.each do 
       if !array[name]
         array[name] = {}
       end
